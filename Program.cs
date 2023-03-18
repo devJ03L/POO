0e884add0e91476bcd94c6f4d76bfec63d81439c
devJ03L <joel.dev.net@gmail.com>
2023-03-17 18:18:21 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Console.WriteLine("Hello, World!");
+
+Fecha f = new Fecha();
+f.setDia(2);
+f.setMes(10);
+f.setAnio(1970);
+
+Console.WriteLine("Dia = " + f.getDia());
+Console.WriteLine("Mes = " + f.getMes());
+Console.WriteLine("Año = " + f.getAnio());
+Console.WriteLine(f);
